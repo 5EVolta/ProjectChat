bf4ba6be2c30452d0a9d8e753040e89c4e02bb28
Dragan <dragan.vule98@gmail.com>
2016-12-12 20:01:07 +0100

    Added Stop Button and messages textArea. Made some other minor changes.

--- src/server/view/View.java
+++ src/server/view/View.java
@@ -1,10 +1,20 @@
 package server.view;
 
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
 import java.awt.Insets;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Locale;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,42 +27,57 @@ import javax.swing.JTextField;
 import server.controller.Controller;
 import server.model.ChatServer;
 import server.model.ChatServerList;
+import sun.font.GraphicComponent;
 
 public class View implements PropertyChangeListener {
 
 	private JFrame f;
 	private JPanel pan;
-	private JLabel labConn, labMess;
-	private JButton butStart, butPort;
-	private JTextArea textConn, textMess;
+	private JLabel labConn, labLog, labMess;
+	private JButton butStart, butStop, butPort;
+	private JTextArea textConn, textLog, textMess;
 	private JTextField textPort;
 	private GridBagConstraints gbc;
 
 	public View(Controller controller) {
 		this.f = new JFrame("Server");
-
+		
 		pan = new JPanel();
 
 		labConn = new JLabel("Active Connections");
-		textConn = new JTextArea(20, 30);
+		textConn = new JTextArea(34, 20);
 		textConn.setWrapStyleWord(true);
 		textConn.setEditable(false);
-		JScrollPane textConnScroll = new JScrollPane(textMess, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+		JScrollPane textConnScroll = new JScrollPane(textConn, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-		labMess = new JLabel("LOG");
-		textMess = new JTextArea(20, 80);
+		labLog = new JLabel("LOG");
+		textLog = new JTextArea(15, 80);
+		textLog.setWrapStyleWord(true);
+		textLog.setEditable(false);
+		JScrollPane textLogScroll = new JScrollPane(textLog, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		
+		labMess = new JLabel("Messagges");
+		textMess = new JTextArea(15, 80);
 		textMess.setWrapStyleWord(true);
 		textMess.setEditable(false);
 		JScrollPane textMessScroll = new JScrollPane(textMess, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
 		butStart = new JButton("Start Server");
+		//butStart.setBackground(Color.WHITE);
+		butStart.setForeground(Color.GREEN);
 		butStart.addActionListener(controller);
 
+		butStop = new JButton("Stop Server");
+		//butStop.setBackground(Color.WHITE);
+		butStop.setForeground(Color.RED);
+		//butStop.addActionListener(controller);
+		
 		butPort = new JButton("OK");
 		textPort = new JTextField("Porta");
-		//textPort.addKeyListener(controller); on day...
+		//textPort.addKeyListener(controller); one day...
 
 		pan.setLayout(new GridBagLayout());
 		gbc = new GridBagConstraints();
@@ -60,39 +85,70 @@ public class View implements PropertyChangeListener {
 		gbc.insets = new Insets(20, 10, 20, 10);
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.LINE_START;
 		pan.add(butStart, gbc);
+		
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		pan.add(butStop, gbc);
 
+		gbc.insets = new Insets(20, 250, 20, 10);
 		gbc.gridx = 1;
-		gbc.ipadx = 80;
+		gbc.gridy = 0;
+		gbc.ipadx = 100;
+		gbc.ipady = 10;
 		gbc.anchor = GridBagConstraints.LINE_END;
 		pan.add(textPort, gbc);
 
+		gbc.insets = new Insets(20, 10, 20, 10);
 		gbc.gridx = 2;
 		gbc.gridy = 0;
+		gbc.ipadx = 100;		
+		gbc.ipady = 5;
 		gbc.anchor = GridBagConstraints.LINE_START;
 		pan.add(butPort, gbc);
 
 		gbc.insets = new Insets(5, 5, 5, 5);
+		gbc.ipadx = 0;	
+		gbc.ipady = 0;
 		gbc.gridx = 0;
 		gbc.gridy = 1;
+		gbc.anchor = GridBagConstraints.LINE_START;
 		pan.add(labConn, gbc);
 
 		gbc.gridx = 1;
-		pan.add(labMess, gbc);
+		gbc.gridy = 1;
+		pan.add(labLog, gbc);
 
 		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.gridx = 0;
 		gbc.gridy = 2;
+		gbc.gridheight = GridBagConstraints.REMAINDER;
 		pan.add(textConnScroll, gbc);
 
 		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbc.gridheight = 1;
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		pan.add(textLogScroll, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+		pan.add(labMess, gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.gridwidth = GridBagConstraints.REMAINDER;
 		pan.add(textMessScroll, gbc);
 
 		f.add(pan);
 
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setLocationRelativeTo(null);
+		f.setLocation(new Point(200,150));
 
 		f.pack();
 		f.setVisible(true);
@@ -111,7 +167,7 @@ public class View implements PropertyChangeListener {
 	}
 
 	public JTextArea getTextMess() {
-		return textMess;
+		return textLog;
 	}
 
 	public JTextField getTextPort() {
